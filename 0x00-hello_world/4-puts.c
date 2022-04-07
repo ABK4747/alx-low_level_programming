8338148b96b2f238fa82d6c266367aefb0272867
ABK4747 <abenaabena194@gmail.com>
2022-04-07 10:05:01 -0700

    msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
- #include <stdio.h>
-
+#include <stdio.h>
 /**
+ * main - entry point
  *
- *  *main - entry point
- *
- *   *
- *
- *    *Return: 0 after printing the function
- *
- *     */
-
+ * Return 0 after printing the function
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts(*\"Programming is like the building a multilingual puzzle");
 	return (0);
-
 }

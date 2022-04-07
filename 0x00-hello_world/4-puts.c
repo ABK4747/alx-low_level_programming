89f9fbe67cd35568abdf3c297d43f999bf8cb341
ABK4747 <abenaabena194@gmail.com>
2022-04-07 07:51:16 -0700

    msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,19 +4,16 @@
  *
  *  * main - main block
  *
- *   *
- *   Return: 0
+ *   * Return: 0
  *
  *    */
 
-int main(void)
-
+int main
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
 
 

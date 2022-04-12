0e9e7da8920dce40720072e6f53840099ebf2179
ABK4747 <abenaabena194@gmail.com>
2022-04-12 07:20:39 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,15 @@
 
 void print_to_98(int n)
 {
-	int count;
-
 	if (n > 98)
+	{
 		for (count = n; count > 98; --count)
-			_putchar("%d, ", count);
+			printf("%d, ", count);
+	}
 	else
+	{
 		for (count = n; count < 98; ++count)
 			_putchar("%d, ", count);
 	printf("98\n");
+	}
 }

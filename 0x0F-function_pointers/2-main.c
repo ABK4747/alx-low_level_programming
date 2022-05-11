dd892a66e572917930fe45a7edb5a9d90d2d22ba
ABK4747 <abenaabena1894@gmail.com>
2022-05-11 05:36:25 -0700

    msg

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -21,6 +21,18 @@ int is_98(int elem)
  * Return: 0 if false, something else otherwise.
  */
 
+int is_strictly_positive(int elem)
+{
+	return (elem > 0);
+}
+
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+
 int abs_is_98(int elem)
 {
 	return (elem == 98 || -elem == 98);
